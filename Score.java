aa562c2566beb2c17152f84947491c9656da46a5
isalevine <isa.loves.coding@gmail.com>
2019-06-26 23:34:04 -0700

    added Score class with three array fields

---
+++ Score.java
@@ -0,0 +1,5 @@
+public class Score {
+    char[] rowOne = new char[]{ ' ', ' ', ' ' };
+    char[] rowTwo = new char[]{ ' ', ' ', ' ' };
+    char[] rowThree = new char[]{ ' ', ' ', ' ' };
+}
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,7 @@
 public class TicTacToe {
     public static void main(String[] args) {
         WelcomeMenu menu = new WelcomeMenu();
+        Score score = new Score();
         
         System.out.println(menu.getText());
     }
